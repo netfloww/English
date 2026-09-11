e55867c6f069704f9f14ae61db6708103368df02
netfloww <wreosh3@gmail.com>
2026-09-11 13:39:48 +0300

    Create voce1.js

---
+++ voce1.js
@@ -0,0 +1,14 @@
+addLesson({
+    id: "voce1",
+    categoryKey: "voce",
+    categoryName: "الصوتيات والنطق",
+    bg: "#f3e8ff",
+    color: "#6b21a8",
+    time: "قراءة 3 دقائق",
+    title: "الدرس الأول: نطق حرف R باللكنة الأمريكية",
+    excerpt: "سر النطق الصحيح لحرف R دون صعوبة.",
+    content: `
+        <p>عند نطق حرف R الأمريكي، قم بثني لسانك للخلف دون أن يلمس سقف الفم.</p>
+        <p><strong>أمثلة:</strong> Car, Water, Learn</p>
+    `
+});
