86182fecc9b098c055bd818434f9acd544af6125
netfloww <wreosh3@gmail.com>
2026-09-11 13:41:04 +0300

    Create mastk1.js

---
+++ mastk1.js
@@ -0,0 +1,15 @@
+addLesson({
+    id: "mastk1",
+    categoryKey: "mastk",
+    categoryName: "الأخطاء الشائعة",
+    bg: "#fee2e2",
+    color: "#dc2626",
+    time: "قراءة 3 دقائق",
+    title: "الفرق بين Say و Tell والتفادي الحرفي للأخطاء",
+    excerpt: "توقف عن الخلط بينهما مع القاعدة البسيطة الشاملة.",
+    content: `
+        <p><strong>الخطأ:</strong> He told that he is tired.</p>
+        <p><strong>الصواب:</strong> He said that he is tired.</p>
+        <p><strong>القاعدة:</strong> كلمة Tell يحب أن يأتي بعدها مفعول به (Tell me / Tell him).</p>
+    `
+});
